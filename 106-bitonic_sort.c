ec3f39c577f8e600142350edee16468bd03c2670
JNart8 <narteyj8@gmail.com>
2023-01-24 23:41:19 +0000

    sorting algorithm

--- 106-bitonic_sort.c
+++ 106-bitonic_sort.c
@@ -1,60 +1,3 @@
-#include "sort.h"
-#include <stdio.h>
-/**
- * swap - change two values in ascending or descending order
- * @arr: array
- * @item1: item one
- * @item2: item two
- * @order: 1: ascending order, 0 descending order
- */
-void swap(int arr[], int item1, int item2, int order)
-{
-	int temp;
-
-	if (order == (arr[item1] > arr[item2]))
-	{
-		temp = arr[item1];
-		arr[item1] = arr[item2];
-		arr[item2] = temp;
-	}
-}
-/**
- * merge - sort bitonic sequences recursively in both orders
- * @arr: array
- * @low: first element
- * @nelemnt: elements number
- * @order: 1: ascending order, 0 descending order
- */
-void merge(int arr[], int low, int nelemnt, int order)
-{
-	int mid, i;
-
-	if (nelemnt > 1)
-	{
-		mid = nelemnt / 2;
-		for (i = low; i < low + mid; i++)
-			swap(arr, i, i + mid, order);
-		merge(arr, low, mid, order);
-		merge(arr, low + mid, mid, order);
-	}
-}
-/**
- * bitonicsort - bitonic sort algorithm implementation
- * @arr: array
- * @low: first element
- * @nelemnt: number of elements
- * @order: 1: ascending order, 0 descending order
- * @size: array lenght
- */
-void bitonicsort(int arr[], int low, int nelemnt, int order, int size)
-{
-	int mid;
-
-	if (nelemnt > 1)
-	{
-		if (order >= 1)
-		{
-			printf("Merging [%i/%i] (UP):\n", nelemnt, size);
 			print_array(&arr[low], nelemnt);
 		}
 		else
@@ -91,3 +34,4 @@ void bitonic_sort(int *array, size_t size)
 		return;
 	bitonicsort(array, 0, size, order, size);
 }
+
